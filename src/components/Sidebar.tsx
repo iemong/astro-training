7a1f1907e5cd04fda35a94f810bc89b38c300ef5
iemong <kiyoshidainagon@gmail.com>
2022-07-28 00:04:59 +0900

    add a react component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Sidebar: React.FC = () => {
+    return (
+        <aside>
+          <p className={'font-bold'}>sidebar</p>
+        </aside>
+    )
+}
+
+export default Sidebar
