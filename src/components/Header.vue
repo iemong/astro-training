5f6dd8bb274d4fba87cf2658b950800b7e9c2871
iemong <kiyoshidainagon@gmail.com>
2022-07-28 22:09:56 +0900

    add header made from vue

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+type Props = {
+    className: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    className: ''
+})
+
+const msg = 'Hello, Vue'
+</script>
+
+<template>
+    <header :class="`bg-[#f00] ${props.className}`">
+        <h1 class="font-bold">{{ msg }}</h1>
+    </header>
+</template>
