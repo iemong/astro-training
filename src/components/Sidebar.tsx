dd652ee5221ec31e38a4dfd8784d1262117e5f04
iemong <kiyoshidainagon@gmail.com>
2022-07-28 09:39:01 +0900

    add click event

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,9 +5,15 @@ type Props = {
 }
 
 const Sidebar: React.FC<Props> = (props: Props) => {
+    
+    const handleClick = React.useCallback(() => {
+      console.log('hogehoge')
+    }, [])
+
     return (
         <aside className={`bg-[#ff0] ${props.className}`}>
           <p className={'font-bold'}>sidebar</p>
+          <button onClick={() => {handleClick()}}>button</button>
         </aside>
     )
 }
