6c5a923bc249e1a7b8c47d41f66f33f649f4de12
iemong <kiyoshidainagon@gmail.com>
2022-07-31 00:34:19 +0900

    add logic

---
+++ src/components/Content.svelte
@@ -0,0 +1,17 @@
+<script>
+    import dayjs from "dayjs";
+    const today = dayjs()
+    let birthday = ''
+    $: diffDays = today.diff(birthday, 'day')
+    $: co2PerDate = 740 //g
+    $: heavy = co2PerDate * diffDays / 1000 / 1000 // t
+</script>
+
+<div>
+    <label>
+        <span>生年月日</span>
+        <input type="date" bind:value={birthday} name="birthday" autocomplete="bday" />
+    </label>
+    <p>{birthday}</p>
+    <p>{heavy}トン</p>
+</div>
