50ab413a80c15e7fdce946e213bf8a99da5a4538
iemong <kiyoshidainagon@gmail.com>
2022-08-01 23:38:52 +0900

    add db

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+import Dexie, { Table } from 'dexie';
+
+export interface User {
+  id?: number;
+  birthDay: string;
+}
+
+export class MySubClassedDexie extends Dexie {
+  // 'friends' is added by dexie when declaring the stores()
+  // We just tell the typing system this is the case
+  friends!: Table<User>; 
+
+  constructor() {
+    super('myDatabase');
+    this.version(1).stores({
+      friends: '++id, birthDay' // Primary key and indexed props
+    });
+  }
+}
+
+export const db = new MySubClassedDexie();
