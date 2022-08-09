5cf26264ea800a4d9f6ca2fbfd6d72a45aa3e636
iemong <kiyoshidainagon@gmail.com>
2022-08-09 23:10:29 +0900

    nanostoresを使ってみる

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,6 +1,7 @@
 <script>
     import { db } from "../db";
     import { liveQuery } from "dexie";
+    import { counter } from "../stores/counter";
 
     import dayjs from "dayjs";
     const today = dayjs();
@@ -20,6 +21,21 @@
     }
 
     let users = liveQuery(() => db.users.toArray());
+
+    let current = 0;
+
+    const unbindListener = counter.subscribe((value) => {
+        console.log("counter value:", value);
+        current = value;
+    });
+
+    const handleClickIncrease = () => {
+        counter.set(counter.get() + 1);
+    };
+
+    const handleClickDecrease = () => {
+        counter.set(counter.get() - 1);
+    };
 </script>
 
 <div>
@@ -33,6 +49,11 @@
             autocomplete="bday"
         />
     </label>
+    <div class="flex gap-2">
+        <button on:click={handleClickDecrease}>-</button>
+        <button on:click={handleClickIncrease}>+</button>
+        <p>current count: {current}</p>
+    </div>
     <p>{birthday}</p>
     <p>{heavy}トン</p>
     <ul>
